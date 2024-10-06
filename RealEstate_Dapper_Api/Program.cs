ceca281e050e3c92909c9899df48498a493f7587
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-06 20:39:49 +0300

    Admin panelinde istatistik bölümünde  bazı işlemleri yapıldı.

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -6,6 +6,7 @@ using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
 using RealEstate_Dapper_Api.Repositories.PopularLocationRepositories;
 using RealEstate_Dapper_Api.Repositories.ProductRepository;
 using RealEstate_Dapper_Api.Repositories.ServiceRepository;
+using RealEstate_Dapper_Api.Repositories.StatisticRepositories;
 using RealEstate_Dapper_Api.Repositories.TestimonialRepositories;
 using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
 
@@ -31,6 +32,7 @@ builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
 builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+builder.Services.AddTransient<IStatisticsRepository, StatisticsRepository>();
 builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
 builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddControllers();
---
+++ RealEstate_Dapper_Api/Repositories/StatisticRepositories/IStatisticsRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace RealEstate_Dapper_Api.Repositories.StatisticRepositories
+{
+    public interface IStatisticsRepository
+    {
+        int CategoryCount();
+        int ActiveCategoryCount();
+        int PassiveCategoryCount();
+        int ProductCount();
+        //productan gelecek
+        int ApartmentCount();
+        string EmployeeNameByMaxProductCount();
+        string CategoryNameByMaxProductCount();
+        decimal AverageProductByRent();
+        decimal AverageProductBySail();
+        // en çok hangi şehirden var
+        string CityNameByMaxProductCount();
+        // kaç farklı şehir var
+        int DifferentCityCount();
+        decimal LastProductPrice();
+        string NewestBuildingYear();
+        string OldestBuildingYear();
+        int AverageRoomCount();
+
+        int ActiveEmployeeCount();
+
+        
+    }
+}
