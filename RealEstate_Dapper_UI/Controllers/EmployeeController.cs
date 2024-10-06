dcb79dec0c787ed31468b1d6fc5569d05b3f0cb2
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-06 19:37:55 +0300

    Admin Panelinden Personel İşlemleri Tamamlandı.

--- RealEstate_Dapper_UI/Controllers/EmployeeController.cs
+++ RealEstate_Dapper_UI/Controllers/EmployeeController.cs
@@ -36,13 +36,53 @@ namespace RealEstate_Dapper_UI.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> CreateCategory(CreateEmployeeDto createEmployeeDto)
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeDto createEmployeeDto)
         {
             var client = _httpClientFactory.CreateClient();
             //Ekleme,Güncelleme işlemleri serialize, Listeleme ve Idye göre getirme Desiriliaze 
             var jsonData = JsonConvert.SerializeObject(createEmployeeDto);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("https://localhost:44304/api/Employees", stringContent);
+            var responseMessage = await client.PostAsync("https://localhost:44304/api/Employes", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44304/api/Employes/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateEmployee(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMesage = await client.GetAsync($"https://localhost:44304/api/Employes/{id}");
+            if (responseMesage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMesage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateEmployeeDto>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDto updateEmployeeDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateEmployeeDto);
+            // apiye göndereceğimiz için stringcontent formatına döndürdük.
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44304/api/Employes/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
