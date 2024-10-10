b00a53cf2bcb9b122900710ef7f6d23492d7c72c
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-10 13:43:26 +0300

    Dashboardda Son 5 İlan Tarihleriyle beraber Listelendi.

--- RealEstate_Dapper_Api/Repositories/ProductRepository/IProductRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/IProductRepository.cs
@@ -10,7 +10,7 @@ namespace RealEstate_Dapper_Api.Repositories.ProductRepository
         void ProductDealOfTheDayStatusChangeToTrue(int id);
         void ProductDealOfTheDayStatusChangeToFalse(int id);
 
-        Task<List<ResultProductDto>> GetLast5ProductAsync();
+        Task<List<ResultLast5ProductWithCategoryDto>> GetLast5ProductAsync();
 
     }
 }
