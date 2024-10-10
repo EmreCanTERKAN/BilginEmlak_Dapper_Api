e6d8ec0653c8d57aa51ea908117a698115832570
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-10 15:53:36 +0300

    DashBoardda Gelen Son 4 Mesajı Listelendi

---
+++ RealEstate_Dapper_UI/ViewCompanents/Dashboard/_DashboardLast4ContactListComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ContactDtos;
+
+namespace RealEstate_Dapper_UI.ViewCompanents.Dashboard
+{
+    public class _DashboardLast4ContactListComponentPartial : ViewComponent
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast4ContactListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        // apiyi consume işlemi yapıyoruz...
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44304/api/Contacts/GetLast4Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<Last4ContactResultDto>>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+    }
+}
