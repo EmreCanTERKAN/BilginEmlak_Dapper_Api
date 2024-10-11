9acfd17ef6431db5f22fbb9b4743e6c544fadab6
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-11 20:30:35 +0300

    Giriş rollerinin karşılığı olarak veri tabanından tek tek bakmamak için bir enum listesi oluşturuldu.

---
+++ RealEstate_Dapper_Api/Enums/RolesType.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate_Dapper_Api.Enums
+{
+
+    // Hangi rol hangisin tek tek bakmaktansa enum oluşturup, tek tek buradan bakabiliriz.
+    public enum RolesType
+    {
+        Admin = 1,
+        Member= 2,
+        Visitor = 3,
+        Manager = 4,
+        Agent = 5
+
+    }
+}
