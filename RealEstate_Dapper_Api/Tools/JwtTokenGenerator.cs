427174451d6ff6fac70482b1ecb9fa1c87f515b8
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-11 17:02:58 +0300

    JWT TOKEN

---
+++ RealEstate_Dapper_Api/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RealEstate_Dapper_Api.Tools
+{
+    public class JwtTokenGenerator
+    {
+        // Sınıf üzerinden newlemeden erişim sağlamak için static tanımlıyoruz.
+        public static TokenResponseViewModel GenerateToken(GetCheckAppUserViewModel model)
+        {
+            var claims = new List<Claim>();
+            if (!string.IsNullOrWhiteSpace(model.Role))
+                claims.Add(new Claim(ClaimTypes.Role, model.Role));
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, model.Id.ToString()));
+
+            if (!string.IsNullOrWhiteSpace(model.Username))
+                claims.Add(new Claim("Username", model.Username));
+
+            //Token oluşturma
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefault.Key));
+
+            // key ile claimsden önce kullanılır. HMACSHA256 bir algoritmadır
+            var signinCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            // Tokenin ayakta kalma süresi
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefault.Expire);
+            //token parametresi
+            JwtSecurityToken token = new JwtSecurityToken(issuer: JwtTokenDefault.ValidIssuer,
+                                                          audience: JwtTokenDefault.ValidAudience,
+                                                          claims: claims, notBefore: DateTime.UtcNow,
+                                                          expires: expireDate,
+                                                          signingCredentials: signinCredentials);
+
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            return new TokenResponseViewModel(tokenHandler.WriteToken(token), expireDate);
+        }
+    }
+}
