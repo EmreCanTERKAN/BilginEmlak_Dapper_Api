50f6358afc4aedb3b15736b77821a90c7cac83f0
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-13 13:19:14 +0300

    Token'ın UI Kısmında Login ile Kullanılması

---
+++ RealEstate_Dapper_UI/Models/JwtResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_UI.Models
+{
+    public class JwtResponseModel
+    {
+        //tokenin kendisini tutar
+        public string Token { get; set; }
+        //kullanım zamanını tututar
+        public DateTime ExpireDate { get; set; }
+    }
+}
