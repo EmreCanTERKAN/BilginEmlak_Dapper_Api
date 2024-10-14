b78888dcf17750ef28540d4dce3807c15525a780
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-14 15:12:01 +0300

    iriş Yapan Emlakçıya Özgü Dashboard Chart Api

---
+++ RealEstate_Dapper_Api/Repositories/EstateAgentRepositories/DashboardRepositories/ChartRepositories/IChartRepository.cs
@@ -0,0 +1,10 @@
+﻿
+using RealEstate_Dapper_Api.Dtos.ChartDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories
+{
+    public interface IChartRepository
+    {
+        Task<List<ResultChartDto>> Get5CityForChart();
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewCompanents/EstateAgent/_EstateAgentDashboardChartComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewCompanents.EstateAgent
+{
+    public class _EstateAgentDashboardChartComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
